1ba8163a1ac24f185dcd9574361d524ad8569691
noedaka <karobka24@gmail.com>
2025-08-02 12:29:54 +0300

    Iteration 6: add request/response  logging

--- go.mod
+++ go.mod
@@ -8,6 +8,11 @@ require (
 )
 
 require (
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0
+)
+
+require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,31 +1,37 @@
 package app
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/noedaka/go-url-shortener/internal/config"
 	"github.com/noedaka/go-url-shortener/internal/handler"
+	"github.com/noedaka/go-url-shortener/internal/logger"
+	"github.com/noedaka/go-url-shortener/internal/middleware"
 	"github.com/noedaka/go-url-shortener/internal/service"
 )
 
 func Run() error {
 	r := chi.NewRouter()
 
-	cfg := config.Init()
+	if err := logger.Init(); err != nil {
+		return err
+	}
+	logger.Log.Sync()
 
+	cfg := config.Init()
 	if err := cfg.ValidateConfig(); err != nil {
 		return err
 	}
 
-	log.Printf("Server is on %s", cfg.ServerAddress)
-	log.Printf("Base URL is %s", cfg.BaseURL)
+	logger.Log.Sugar().Infof("Server is on %s", cfg.ServerAddress)
+	logger.Log.Sugar().Infof("Base URL is %s", cfg.BaseURL)
 
 	service := service.NewURLStorage()
 	handlerURL := handler.NewHandler(service, cfg.BaseURL)
 
 	r.Route("/", func(r chi.Router) {
+		r.Use(middleware.LoggingMiddleware)
 		r.Post("/*", handlerURL.ShortenURLHandler)
 		r.Get("/{id}", handlerURL.ShortIDHandler)
 	})
