c84d3d74561104b10f28d1eec358f590da874909
noedaka <karobka24@gmail.com>
2025-08-08 20:46:40 +0300

    Iteration 7: add JSON shortener handler

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/noedaka/go-url-shortener/internal/model"
 	"github.com/noedaka/go-url-shortener/internal/service"
 )
 
@@ -20,7 +22,8 @@ func NewHandler(service service.URLStorer, baseURL string) *Handler {
 func (h *Handler) ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Error reading body", http.StatusBadRequest)
+		http.Error(w, "cannot read body", http.StatusBadRequest)
+		return
 	}
 
 	defer r.Body.Close()
@@ -29,7 +32,7 @@ func (h *Handler) ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
 
 	shortID, err := h.service.ShortenURL(originalURL)
 	if err != nil {
-		http.Error(w, "Error shortening url", http.StatusBadRequest)
+		http.Error(w, "cannot shorten url", http.StatusBadRequest)
 		return
 	}
 
@@ -40,12 +43,42 @@ func (h *Handler) ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(shortURL))
 }
 
+func (h *Handler) APIShortenerHandler(w http.ResponseWriter, r *http.Request) {
+	var req model.Request
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "cannot decode request JSON body", http.StatusBadRequest)
+		return
+	}
+
+	shortID, err := h.service.ShortenURL(req.URL)
+	if err != nil {
+		http.Error(w, "cannot shorten url", http.StatusBadRequest)
+		return
+	}
+
+	shortURL := h.baseURL + "/" + shortID
+
+	resp := model.Response{
+		Result: shortURL,
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(resp); err != nil {
+		http.Error(w, "error encoding response", http.StatusBadRequest)
+		return
+	}
+}
+
 func (h *Handler) ShortIDHandler(w http.ResponseWriter, r *http.Request) {
 	shortID := chi.URLParam(r, "id")
 
 	URL, err := h.service.GetURL(shortID)
 	if err != nil {
-		http.Error(w, "Error getting url from id", http.StatusBadRequest)
+		http.Error(w, "cannot get url from id", http.StatusBadRequest)
 		return
 	}
 
